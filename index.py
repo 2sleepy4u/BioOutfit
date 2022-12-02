449446e5b769ea07f94ef9fbcf781ca6f4b5fa40
2sleepy4u <riki.zancan@gmail.com>
2022-12-02 12:29:15 +0100

    Add files via upload

--- index.py
+++ index.py
@@ -1,20 +1,37 @@
-from flask import Flask
+from collections import namedtuple
+from glob import escape
+from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Qui verrà utilizzato l'algoritmo!"
+    return "HOME"
 
-@app.route('/impostazioni')
-def impostazioni():
-    return "Qui verranno modificate impostazioni varie!"
+@app.route('/generate/<fashion>')
+def outfit(fashion):
+    return f"Fashion level {escape(fashion)}"
 
-@app.route('/inserisci')
-def inserisci():
-    return "Qui verranno inseriti i vestiti!"
 
-@app.route('/modifica')
+# INSERT
+@app.get('/insert')
+def get_insert():
+    data = {}
+    data["name"]        = request.args.get('name', 'untitled')
+    data["fashion"]     = request.args.get('fashion', 6)
+    data["warmness"]    = request.args.get('warmness', 6)
+    data = json.dumps(data)
+    return f"{data}"
+
+@app.post('/insert')
+def post_insert():
+    data = request.get_json()
+    data = json.dumps(data)
+    return f'{data}'
+
+
+@app.route('/update', methods=["POST", "GET"])
 def modifica():
     return "Qui verranno modificati i vestiti!"
 
